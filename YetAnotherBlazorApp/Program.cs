c54194a97db73cf583b523a69ff33212590d66db
ecabigting <ericcabigting@outlook.com>
2022-01-28 19:34:43 +0400

    update gitignore

--- YetAnotherBlazorApp/Program.cs
+++ YetAnotherBlazorApp/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using YetAnotherBlazorApp;
 
+Console.WriteLine("Yeah yeah.. its running..");
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
